b5faa645c0dc1e49baf8590012df6fa43827c987
GreatBryan <11813011@mail.sustech.edu.cn>
2022-03-22 10:41:42 +0800

    m

---
+++ OnlinePlatform/src/main/java/com/platform/model/User.java
@@ -0,0 +1,54 @@
+package com.platform.model;
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String account;
+
+    private String password;
+
+    private int role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+}
